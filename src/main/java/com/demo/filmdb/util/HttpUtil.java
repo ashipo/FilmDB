e109828dbc074bb42262903b3ac853fa39820302
ashipo <24320267+ashipo@users.noreply.github.com>
2024-06-03 19:42:50 +0300

    Create HttpUtil.require function
    Function throws 404 if provided value is null

---
+++ src/main/java/com/demo/filmdb/util/HttpUtil.java
@@ -0,0 +1,25 @@
+package com.demo.filmdb.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.annotation.Nullable;
+import java.util.function.Supplier;
+
+public final class HttpUtil {
+
+    /**
+     * Throws 404 if {@code entity} is {@code null} or returns it otherwise.
+     *
+     * @param entity to check.
+     * @param message error message supplier.
+     * @return the entity.
+     * @throws ResponseStatusException {@link HttpStatus#NOT_FOUND} if {@code entity} is {@code null}.
+     */
+    public static <T> T require(@Nullable T entity, Supplier<String> message) {
+        if (entity == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, message.get());
+        }
+        return entity;
+    }
+}
