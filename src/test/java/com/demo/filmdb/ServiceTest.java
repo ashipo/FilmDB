896aa97c864c7ae11e6e4fe67c9c5f71c640c7e9
ashipo <24320267+ashipo@users.noreply.github.com>
2024-06-05 19:24:58 +0300

    Cleanup ServiceTest

--- src/test/java/com/demo/filmdb/ServiceTest.java
+++ src/test/java/com/demo/filmdb/ServiceTest.java
@@ -8,9 +8,6 @@ import com.demo.filmdb.role.RoleRepository;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.web.server.ResponseStatusException;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public abstract class ServiceTest {
@@ -21,13 +18,6 @@ public abstract class ServiceTest {
     protected PersonRepository personRepository;
     @Mock
     protected RoleRepository roleRepository;
-
-    protected void assertThatValid404(Throwable thrown){
-        assertThat(thrown).as("Expected ResponseStatusException to be thrown")
-                .isInstanceOf(ResponseStatusException.class);
-        ResponseStatusException statusException = (ResponseStatusException) thrown;
-        assertThat(statusException.getStatusCode().value()).as("Status code").isEqualTo(404);
-    }
 
     protected Film createFilm(long id) {
         Film result = new Film();
