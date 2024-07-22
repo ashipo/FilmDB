a0a09113b121ad9ea1957ef9f4737f1375a04ef8
ashipo <24320267+ashipo@users.noreply.github.com>
2024-07-22 18:49:35 +0300

    Make FilmDtoInput.synopsis @Nullable

--- src/main/java/com/demo/filmdb/film/dtos/FilmDtoInput.java
+++ src/main/java/com/demo/filmdb/film/dtos/FilmDtoInput.java
@@ -3,6 +3,7 @@ package com.demo.filmdb.film.dtos;
 import com.demo.filmdb.film.Film;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.lang.Nullable;
 
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -11,5 +12,8 @@ import java.time.LocalDate;
  * A DTO for the {@link Film} entity.
  * Intended use: request.
  */
-public record FilmDtoInput(@NotEmpty String title, @NotNull LocalDate releaseDate, String synopsis) implements Serializable {
+public record FilmDtoInput(
+        @NotEmpty String title,
+        @NotNull LocalDate releaseDate,
+        @Nullable String synopsis) implements Serializable {
 }
