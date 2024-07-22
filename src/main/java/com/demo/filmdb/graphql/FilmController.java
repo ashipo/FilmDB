4419f010330e6e4695ccba40085fe2a226bccc32
ashipo <24320267+ashipo@users.noreply.github.com>
2024-07-22 17:10:59 +0300

    Refactor updateFilm in GraphQL controller
    Make it use of FilmService.updateFilm

--- src/main/java/com/demo/filmdb/graphql/FilmController.java
+++ src/main/java/com/demo/filmdb/graphql/FilmController.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -49,14 +48,11 @@ public class FilmController {
     }
 
     @MutationMapping
-    @PreAuthorize("hasRole('ADMIN')")
     public Film updateFilm(@Argument Long id, @Argument FilmInput input) {
-        final Film film = filmService.getFilm(id);
-        if (film == null) {
-            return null;
-        }
+        final Film film = new Film();
+        film.setId(id);
         updateFilmFromInput(film, input);
-        return filmService.saveFilm(film);
+        return filmService.updateFilm(film);
     }
 
     @MutationMapping
