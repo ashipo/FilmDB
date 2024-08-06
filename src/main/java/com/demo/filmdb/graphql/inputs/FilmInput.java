7b2e26b691cdb65c079617819437f5f89c885060
ashipo <24320267+ashipo@users.noreply.github.com>
2024-08-06 20:56:27 +0300

    Specify nullability in FilmInput

--- src/main/java/com/demo/filmdb/graphql/inputs/FilmInput.java
+++ src/main/java/com/demo/filmdb/graphql/inputs/FilmInput.java
@@ -1,6 +1,8 @@
 package com.demo.filmdb.graphql.inputs;
 
+import org.springframework.lang.Nullable;
+
 import java.time.LocalDate;
 
-public record FilmInput(String title, LocalDate releaseDate, String synopsis) {
+public record FilmInput(String title, LocalDate releaseDate, @Nullable String synopsis) {
 }
