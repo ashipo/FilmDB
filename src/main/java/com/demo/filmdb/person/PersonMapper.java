529cfe3ad3e725d68bbe0f1556ba01e1703098b8
ashipo <24320267+ashipo@users.noreply.github.com>
2024-08-13 17:45:06 +0300

    Add mapping from PersonInfo to Person to PersonMapper

--- src/main/java/com/demo/filmdb/person/PersonMapper.java
+++ src/main/java/com/demo/filmdb/person/PersonMapper.java
@@ -1,20 +1,14 @@
 package com.demo.filmdb.person;
 
 import com.demo.filmdb.person.dtos.PersonDto;
-import com.demo.filmdb.person.dtos.PersonDtoInput;
 import org.mapstruct.*;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public interface PersonMapper {
 
-    /* PersonDto */
-
     PersonDto personToPersonDto(Person person);
 
-    /* PersonDtoInput */
-
-    Person personDtoInputToPerson(PersonDtoInput personDtoInput);
-
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    Person updatePersonFromPersonDtoInput(PersonDtoInput personDtoInput, @MappingTarget Person person);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
+    @Mapping(source = "dateOfBirth", target = "dob")
+    void updatePersonFromPersonInfo(PersonInfo personInfo, @MappingTarget Person person);
 }
