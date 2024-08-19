da333621af6b558eecc937c8119617978208d37b
ashipo <24320267+ashipo@users.noreply.github.com>
2024-08-19 20:47:22 +0300

    Cleanup FilmMapper

--- src/main/java/com/demo/filmdb/film/FilmMapper.java
+++ src/main/java/com/demo/filmdb/film/FilmMapper.java
@@ -1,19 +1,12 @@
 package com.demo.filmdb.film;
 
 import com.demo.filmdb.film.dtos.FilmDto;
-import com.demo.filmdb.film.dtos.FilmDtoInput;
 import org.mapstruct.*;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public interface FilmMapper {
 
-    /* FilmDto */
-
     FilmDto filmToFilmDto(Film film);
-
-    /* FilmDtoInput */
-
-    Film filmDtoInputToFilm(FilmDtoInput filmDtoInput);
 
     Film filmInfoToFilm(FilmInfo filmInfo);
 
